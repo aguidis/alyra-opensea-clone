062074c1309668de9d4247c9ca038533ea93c9fd
Adrien Guidis <adrien.guidis@outlook.com>
2022-05-30 09:01:33 +0200

    add nft contracts + scripts

--- migrations/1_deploy_dapptoken.js
+++
@@ -1,6 +0,0 @@
-const DappToken = artifacts.require('DappToken')
-
-module.exports = async function (deployer, network, accounts) {
-    // Deploy Dapp Token
-    await deployer.deploy(DappToken)
-}
